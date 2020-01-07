19637028ca2e31c279f2ac4675018eaae565088c
Tadas <vtadas25@gmail.com>
2020-01-07 08:25:32 +0200

    Add test.sh script

---
+++ mod_intervalinis_kodas/final/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BITS=12
+CODING="c1"
+
+source_file="test-files/$1"
+archive="test-files/output.bin"
+decoded="test-files/decoded.txt"
+
+echo -n "Encoding..."
+time ./encode $BITS $CODING $source_file
+
+echo -n "Decoding..."
+time ./decode $archive
+
+echo "Original: "
+ls -l $source_file
+sha256sum $source_file
+echo "Decoded: "
+ls -l $decoded
+sha256sum $decoded
+echo "Archive: "
+ls -l $archive
